40b18090f6c0fd0698251bc7d3f32fffd8284ac7
George Luiz Bittencourt <george@georgeluiz.com>
2024-08-26 13:12:50 -0300

    changes.

--- install.sh
+++ install.sh
@@ -4,4 +4,8 @@
 sudo apt update && sudo apt upgrade
 
 # install packages.
-sudo apt install -y lsd curl
+sudo apt install -y lsd curl neovim bat
+
+# tune the .basrhrc file.
+echo "alias ls='lsd'" >> ~/.bashrc
+echo "alias cat='batcat'" >> ~/.bashrc
